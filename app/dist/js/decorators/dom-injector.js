9d93df6b918fb1d46387d648823f348c3e3455fd
KatiaSonally <katiasonally@yahoo.com.br>
2023-07-29 20:33:02 -0300

    feat: turns the properties the decorator was applied to into getters

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,4 +1,7 @@
 export function domInjector(setor) {
     return function (target, propertyKey) {
+        const getter = function () {
+            const elemento = document.querySelector;
+        };
     };
 }
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,5 +1,9 @@
 export function domInjector(setor: string) {
     return function(target: any, propertyKey: string) {
-        
+
+        const getter = function() {
+            const elemento = document.querySelector(seletor);
+            return elemento;
+        }
     }
 }
