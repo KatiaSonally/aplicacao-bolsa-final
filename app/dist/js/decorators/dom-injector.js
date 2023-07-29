9b07f86a891eb0d6fdb6b3a5dad65b9842b24ef8
KatiaSonally <katiasonally@yahoo.com.br>
2023-07-29 20:49:40 -0300

    feat: makes implementations for the decorator to fetch elements in the DOM

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,7 +1,13 @@
-export function domInjector(setor) {
+export function domInjector(seletor) {
     return function (target, propertyKey) {
+        console.log(`Modificando prototype ${target.constructor.name}
+        e adicionando getter para a propriedade ${propertyKey}`);
         const getter = function () {
-            const elemento = document.querySelector;
+            const elemento = document.querySelector(seletor);
+            console.log(`buscando elemento do DOM como seletor
+            ${seletor} para injetar em ${propertyKey}`);
+            return elemento;
         };
+        Object.defineProperty(target, propertyKey, { get: getter });
     };
 }
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,9 +1,18 @@
-export function domInjector(setor: string) {
+export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
-
+        console.log(`Modificando prototype ${target.constructor.name}
+        e adicionando getter para a propriedade ${propertyKey}`)
         const getter = function() {
             const elemento = document.querySelector(seletor);
+            console.log(`buscando elemento do DOM como seletor
+            ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         }
+
+        Object.defineProperty(
+            target, 
+            propertyKey, 
+            { get: getter }
+        );
     }
 }
