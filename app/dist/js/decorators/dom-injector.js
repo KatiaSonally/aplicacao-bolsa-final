6528b94154108e86588db77cb67df6d73d486b9f
KatiaSonally <katiasonally@yahoo.com.br>
2023-07-30 18:32:07 -0300

    feat: implements cache in the decorator

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,11 +1,14 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
         console.log(`Modificando prototype ${target.constructor.name}
-        e adicionando getter para a propriedade ${propertyKey}`);
+            e adicionando getter para a propriedade ${propertyKey}`);
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM como seletor
-            ${seletor} para injetar em ${propertyKey}`);
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`buscando elemento do DOM como seletor
+                ${seletor} para injetar em ${propertyKey}`);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
